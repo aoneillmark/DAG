7347346d8136e1c0bfa2fffea685c8b095befa43
Mark O'Neill <aoneillmark@gmail.com>
2024-11-13 18:15:25 +0000

    first crack

--- solve_dag.py
+++ solve_dag.py
@@ -1,2 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+class Node:
+    name: str
+    p: int
+    d: int
+
+
+class DAG:
+    nodes: tree[Node]
+
+
+    def __init__(self, node: Node) -> None:
+        self.nodes = [node]
+
+
+    def C(self):
+        return sum([node.p for node in self.nodes])
+
+
+    def g(self, j: int):
+        return max(0, self.C() - self.nodes[j].d)
+
+
+    def pick_next(self) -> Node:
+        node: Node = min( [self.g(j) for j in range(self.len())] )
+
+
+def main():
+    return
