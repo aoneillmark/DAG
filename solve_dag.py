55dc9ea79c8c362f19566647c107f958a5b6374f
Arneaj <arnaud.rollandmail@gmail.com>
2024-11-14 14:07:16 +0000

    added predecessors and succesors to Nodes + a couple things

--- solve_dag.py
+++ solve_dag.py
@@ -6,26 +6,38 @@ class Node:
     name: str
     p: int
     d: int
+    predecessors: list
+    successors: list
+
+    def __init__(self, name: str, p: int, d: int, predecessors: list = [], successors: list = []):
+        self.name= name
+        self.p = p
+        self.d = d
+        self.predecessors = predecessors
+        self.successors = successors
 
 
 class DAG:
-    nodes: tree[Node]
+    nodes: list[Node]
 
 
-    def __init__(self, node: Node) -> None:
+    def __init__(self, node: Node):
         self.nodes = [node]
 
 
-    def C(self):
+    def C(self) -> int:
         return sum([node.p for node in self.nodes])
 
 
-    def g(self, j: int):
+    def g(self, j: int) -> int:
         return max(0, self.C() - self.nodes[j].d)
 
 
-    def pick_next(self) -> Node:
-        node: Node = min( [self.g(j) for j in range(self.len())] )
+    def pick_next(self, schedule: list[Node]) -> Node:
+        min_index: int = min([self.g(j) for j in range(self.nodes.len())])
+        
+        schedule.insert(0, self.nodes[min_index])
+        self.nodes.pop(min_index)
 
 
 def main():
