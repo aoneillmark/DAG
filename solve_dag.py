a2155d365775ab35ff3d421e49433cafbf246f03
Arneaj <arnaud.rollandmail@gmail.com>
2024-11-14 14:38:20 +0000

    remove the need for a DAG class as I used a python DAG library, improved some functions

--- solve_dag.py
+++ solve_dag.py
@@ -1,44 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from paradag import DAG
 
 
 class Node:
     name: str
     p: int
     d: int
-    predecessors: list
-    successors: list
 
-    def __init__(self, name: str, p: int, d: int, predecessors: list = [], successors: list = []):
-        self.name= name
+    def __init__(self, name: str, p: int, d: int, predecessors: np.ndarray = [], successors: np.ndarray = []):
+        self.name = name
         self.p = p
         self.d = d
-        self.predecessors = predecessors
-        self.successors = successors
 
 
-class DAG:
-    nodes: list[Node]
+
+def C(l_nodes: DAG, l_schedule: np.ndarray) -> int:
+    return sum([node.p for node in l_nodes.vertices() if node not in l_schedule])
 
 
-    def __init__(self, node: Node):
-        self.nodes = [node]
+def g(l_nodes: DAG, j: int) -> int:
+    return max(0, C(l_nodes) - l_nodes[j].d)
+
+def g(l_nodes: DAG, node: Node) -> int:
+    return max(0, C(l_nodes) - node.d)
 
 
-    def C(self) -> int:
-        return sum([node.p for node in self.nodes])
+def pick_next(l_nodes: DAG, l_schedule: np.ndarray[Node]) -> Node:
+    min_index: int = min([g(l_nodes, j) 
+                          for j in range(l_nodes.vertices().len()) 
+                          if l_nodes[j] not in l_schedule 
+                          and l_nodes.successors(l_nodes[j]) in l_schedule ])
+    
+    l_schedule.insert(0, l_nodes.vertices()[min_index])
 
-
-    def g(self, j: int) -> int:
-        return max(0, self.C() - self.nodes[j].d)
-
-
-    def pick_next(self, schedule: list[Node]) -> Node:
-        min_index: int = min([self.g(j) for j in range(self.nodes.len())])
-        
-        schedule.insert(0, self.nodes[min_index])
-        self.nodes.pop(min_index)
 
 
 def main():
-    return
+    nodes = DAG()
+    schedule = np.empty()
