ffc81bd8ead79fe62a3739751fc819ca2a9c91a1
Arneaj <arnaud.rollandmail@gmail.com>
2024-11-15 11:06:53 +0000

    question 1.2 done

--- solve_dag.py
+++ solve_dag.py
@@ -23,23 +23,33 @@ def g(l: list, l_schedule: np.ndarray, j: int) -> int:
     return max(0, C(l, l_schedule) - l[j].d)
 
 
-def pick_next(l_nodes: DAG, l_schedule: np.ndarray, current_index):
+def pick_next(l_nodes: DAG, l_schedule: list):
     set_copy = []
     for node in l_nodes.vertices():
         set_copy.append( node )
     
-    min_index: int = 0
-    min_g: int = 1000000000
+    min_index: int = 100000000000
+    min_g: int = 100000000000
 
     for i in range(len(set_copy)):
-        if (set_copy[i] not in l_schedule) \
-                and (l_nodes.successors(set_copy[i]) in l_schedule or l_nodes.successors(set_copy[i]) == []) \
-                and (g(set_copy, l_schedule, i) < min_g):
-            min_index = i
-            min_g = g(set_copy, l_schedule, i)
+        if (set_copy[i].name not in [node.name for node in l_schedule]):
+            if l_nodes.successors(set_copy[i]) == set():
+                if (g(set_copy, l_schedule, i) < min_g):
+                    min_index = i
+                    min_g = g(set_copy, l_schedule, i)
+            
+            all_successors_in_schedule = True
+            for node in l_nodes.successors(set_copy[i]):
+                if node not in l_schedule:
+                    all_successors_in_schedule = False
+            
+            if all_successors_in_schedule:
+                if (g(set_copy, l_schedule, i) < min_g):
+                    min_index = i
+                    min_g = g(set_copy, l_schedule, i)
+            
 
-    l_schedule[current_index] = set_copy[min_index]
-
+    l_schedule.insert(0, set_copy[min_index])
 
 
 
@@ -48,17 +58,19 @@ def main():
     J2 = Node("J2", 4, 1)
     J3 = Node("J3", 2, 3)
     J4 = Node("J4", 1, 4)
-    
+
     nodes = DAG()
     nodes.add_vertex(J1, J2, J3, J4)
     nodes.add_edge(J1, J3)
     nodes.add_edge(J3, J4)
     nodes.add_edge(J2, J4)
     
-    schedule = np.empty(len(nodes.vertices()), Node)
+    schedule = []
     
     for i in range(len(nodes.vertices())-1, -1, -1):
-        pick_next(nodes, schedule, i)
+        pick_next(nodes, schedule)
     
     for node in schedule:
         print(node.name)
+
+main()
